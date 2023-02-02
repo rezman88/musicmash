48928ced7ae10fc48dee8cb687678cffac76ba31
Jose Alonso <jose.m.alonso.g@gmail.com>
2023-02-02 21:27:46 +0000

    adding list of best tracks

--- script.js
+++ script.js
@@ -43,3 +43,49 @@ $("#search-form").on("submit", function (event) {
     });
   });
 });
+
+
+
+
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "df2546054bmshe9048d5ae9a1441p13d910jsn20aabd33d717",
+		"X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
+	}
+};
+
+
+
+var dataSearch
+
+$.ajax(settings).done(function (response) {
+	dataSearch = response;
+  console.log(response);
+  $("#best-albums").empty();
+  $("#best-albums").append("<h2>Best tracks</h2>");
+  for (let i = 0; i < dataSearch.data.length; i++) {
+    var trackId = dataSearch.data[i].id;
+    var trackTitle = dataSearch.data[i].title;
+    var trackPreview = dataSearch.data[i].preview;
+    var trackRank = dataSearch.data[i].rank;
+    var trackAlbumId = dataSearch.data[i].album.id;
+    var trackAlbumTitle = dataSearch.data[i].album.title
+    var trackAlbumImage = dataSearch.data[i].album.cover_medium;
+    $("#best-albums").append(`
+      <p>Title: ${trackTitle} </p>
+      <p>${trackPreview} </p>
+      <p>Album: ${trackAlbumTitle} </p>
+      <p><img src="${trackAlbumImage}" alt="${trackAlbumImage}"> </p>
+    `);
+  }
+  
+});
+
+
+
+
